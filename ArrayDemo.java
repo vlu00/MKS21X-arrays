f3cbb346f73b849f6640a5d8556eca7d45dbdd7e
vlu00 <vlu00@stuy.edu>
2018-09-26 19:12:14 -0400

    printArray for 2D arrays is tested and working

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -4,8 +4,19 @@ public class ArrayDemo{
       System.out.print(ary[i]);
     }
   }
+  public static void printArray(int[][]ary){
+    for (int r = 0; r < ary.length; r++) {
+      for (int c = 0; c < ary[r].length; c++) {
+        System.out.print(ary[r][c]);
+      }
+    System.out.println();
+    }
+  }
   public static void main (String [] args) {
-    int[] test = {1, 2, 3, 4};
-    printArray(test);
+    int[] testA = {1, 2, 3, 4};
+    int [] [] testB = new int [] [] { {0,1,0,1}, {1,0,1,0} };
+    printArray(testA);
+    System.out.println();
+    printArray(testB);
   }
 }
