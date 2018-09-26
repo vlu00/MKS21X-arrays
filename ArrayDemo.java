d3f34f48bcd132c4df5fd952cfbb62188dd01bcd
vlu00 <vlu00@stuy.edu>
2018-09-26 19:36:59 -0400

    countZeros2D tested and working

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -4,6 +4,7 @@ public class ArrayDemo{
       System.out.print(ary[i]);
     }
   }
+
   public static void printArray(int[][]ary){
     for (int r = 0; r < ary.length; r++) {
       for (int c = 0; c < ary[r].length; c++) {
@@ -12,11 +13,25 @@ public class ArrayDemo{
     System.out.println();
     }
   }
+
+  public static int countZeros2D(int[][] nums){
+    int x = 0;
+    for (int r = 0; r < nums.length; r++) {
+      for (int c = 0; c < nums[r].length; c++) {
+        if (nums[r][c] == 0) {
+          x++;
+        }
+      }
+    }
+    return x;
+  }
+
   public static void main (String [] args) {
     int[] testA = {1, 2, 3, 4};
     int [] [] testB = new int [] [] { {0,1,0,1}, {1,0,1,0} };
     printArray(testA);
     System.out.println();
     printArray(testB);
+    System.out.print (countZeros2D(testB));
   }
 }
